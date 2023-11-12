8887b1ada4ca5af7e443779b00c835bdaf1e2046
AbdallahAli2 <119799728+AbdallahAli2@users.noreply.github.com>
2023-11-12 16:01:02 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import NavBar from "./components/NavBar";
+import AboutMe from "./components/AboutMe";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
